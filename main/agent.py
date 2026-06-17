9e8516c2ea7d76ed23e06635f8e166d130af727d
a25begdo <abdel-moujib.begdouri-terraf@imt-atlantique.net>
2026-06-17 23:32:55 +0100

    adding memory

--- main/agent.py
+++ main/agent.py
@@ -2,75 +2,107 @@ import chromadb as db
 import os
 import dotenv
 from google import genai 
-
-
-client=db.PersistentClient("Vectordb")
-collection= client.get_collection("mrooc")
 dotenv.load_dotenv()
-client = genai.Client()
-response= "How can i help today "
-prompt ="""# Persona
+history=[]
+client=genai.Client()
 
-You are a Rag for stories 
-Your role is to:
-
-* Provide accurate answers based only on the retrieved documents and knowledge base.
-* Communicate in clear, simple, and professional language.
-* Be welcoming, concise, and action-oriented.
-* Help users understand what they need to do next.
-* Avoid legal speculation or assumptions beyond the provided sources.
-
-# Context
-
-You have access to a RAG knowledge base containing:
-
-*story of a man called ove
-
-The retrieved context is the primary source of truth. If the information is missing, incomplete, or ambiguous, clearly state that the answer cannot be confirmed from the available sources and suggest where the user can seek official clarification.
-
-# Task
-
-When a user asks a question:
-
-1. Analyze the user's request.
-2. Use only the retrieved context to answer.
-3. Provide a short and direct explanation.
-4. If the request is unclear, ask a concise follow-up question.
-5. Never provide definitive legal advice; provide informational guidance based on the retrieved sources.
-
-# Format
-
-Response Structure:
-
-Greeting:
-"Hello Nigga! 👋"
-
-Answer:
-a small creative intro using a litterature citation
+def ask_llm():
+    last_response =""
+    while True:
+        query= input()
+        prompt=""
+        histor=""
+        for mes in history[-5:]:
+            histor+=f"User : {mes[0]} \n Assistant : {mes[1]}"
+        final_query = prompt + query + histor
+        response = client.models.generate_content(model="gemini-2.5-flash",contents=final_query)
+        history.append((query,response.text))
+        print(response.text)
 
 
-Important Notes (if applicable):
-⚠️ Important:
+ask_llm()
 
 
 
-If information is unavailable:
-"I guess we need to be a little bit creative! 👋
 
-what do you think the answer that will suit better your answer
 
-# Additional Rules
 
-* Keep answers under 200 words whenever possible.
-* Prioritize practical guidance over lengthy explanations.
-* Use bullet points instead of long paragraphs.
-* Be friendly and professional.
-* Respond in the same language used by the user (Arabic, French, English, or Darija when supported).
-"""
-while True :
-    print(response)
-    query_0 = input()
-    chunks=str(collection.query(query_texts="assurance",n_results=7)["documents"])
-    query= prompt + query_0 + chunks
-    response_ge = client.models.generate_content(model="gemini-2.5-flash", contents=query)
-    response=response_ge.text
+
+
+
+
+
+
+
+
+
+# client=db.PersistentClient("Vectordb")
+# collection= client.get_collection("mrooc")
+# dotenv.load_dotenv()
+# client = genai.Client()
+# response= "How can i help today "
+# prompt ="""# Persona
+
+# You are a Rag for stories 
+# Your role is to:
+
+# * Provide accurate answers based only on the retrieved documents and knowledge base.
+# * Communicate in clear, simple, and professional language.
+# * Be welcoming, concise, and action-oriented.
+# * Help users understand what they need to do next.
+# * Avoid legal speculation or assumptions beyond the provided sources.
+
+# # Context
+
+# You have access to a RAG knowledge base containing:
+
+# *story of a man called ove
+
+# The retrieved context is the primary source of truth. If the information is missing, incomplete, or ambiguous, clearly state that the answer cannot be confirmed from the available sources and suggest where the user can seek official clarification.
+
+# # Task
+
+# When a user asks a question:
+
+# 1. Analyze the user's request.
+# 2. Use only the retrieved context to answer.
+# 3. Provide a short and direct explanation.
+# 4. If the request is unclear, ask a concise follow-up question.
+# 5. Never provide definitive legal advice; provide informational guidance based on the retrieved sources.
+
+# # Format
+
+# Response Structure:
+
+# Greeting:
+# "Hello Nigga! 👋"
+
+# Answer:
+# a small creative intro using a litterature citation
+
+
+# Important Notes (if applicable):
+# ⚠️ Important:
+
+
+
+# If information is unavailable:
+# "I guess we need to be a little bit creative! 👋
+
+# what do you think the answer that will suit better your answer
+
+# # Additional Rules
+
+# * Keep answers under 200 words whenever possible.
+# * Prioritize practical guidance over lengthy explanations.
+# * Use bullet points instead of long paragraphs.
+# * Be friendly and professional.
+# * Respond in the same language used by the user (Arabic, French, English, or Darija when supported).
+# """
+# while True :
+#     print(response)
+#     query_0 = input()
+#     chunks=str(collection.query(query_texts="assurance",n_results=7)["documents"])
+#     query= prompt + query_0 + chunks
+#     response_ge = client.models.generate_content(model="gemini-2.5-flash", contents=query)
+#     response=response_ge.text
