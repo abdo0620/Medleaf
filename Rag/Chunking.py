0a3759a8217152609bbd17207dbf234adcf6dfe4
a25begdo <abdel-moujib.begdouri-terraf@imt-atlantique.net>
2026-06-21 18:39:42 +0100

    Improving the code

--- Rag/Chunking.py
+++ Rag/Chunking.py
@@ -1,5 +1,3 @@
-from google import genai
-import os  
 import dotenv
 from google.genai.local_tokenizer import LocalTokenizer
 
@@ -7,16 +5,20 @@ dotenv.load_dotenv()
 sep = ["\n\n","\n",".",","]
 tokenizer = LocalTokenizer(model_name="gemini-2.5-flash")
 
-def closest_space(text,overlap):
+def closest_space(text:str,overlap:float):
+    assert 0 <= overlap and overlap <= 1,"overlap value must be bettween 0 and 1"
     n=text.split(" ")
     m = int(overlap*len(n))
     new_text=""
-    for i in range(len(n)-m-1,len(n)):
+    for i in range(len(n)-m,len(n)):
         new_text+= n[i] + " "
     return new_text.strip()
-print(closest_space("fjf fjf jf f fjf f f fjf f  ggggggggggg",0.6))
+print(closest_space("fjf fjf jf f fjf f f fjf f  ggggggggggg",3))
 
 def overlaping(chunks,overlap ):
+    assert 0 <= overlap and overlap <= 1,"overlap value must be bettween 0 and 1"
+    if chunks==[]:
+        return []
     new_chunks=[chunks[0]]
     for i in range(1,len(chunks)):
         new_chunks.append(closest_space(chunks[i-1],overlap)+" | " +chunks[i])
--- Rag/retreival.py
+++ Rag/retreival.py
@@ -3,6 +3,7 @@ import chromadb as db
 client=db.PersistentClient("Vectordb")
 collection =client.get_collection("mrooc")
 
-def retreive_chunks(query):
+def retreive_chunks(query:str):
+
     results=collection.query(query_texts=query,n_results=5)["documents"]
     return results
