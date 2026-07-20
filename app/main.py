23d90cb6c8822492d4c29dcdd9f2c33f5bfb7587
a25begdo <abdel-moujib.begdouri-terraf@imt-atlantique.net>
2026-07-20 14:52:05 +0100

    feat: add main navigation page for the MedLeaf app

---
+++ app/main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+Home=st.Page("app.py",title="Talk To Mia", icon="🩺")
+Upload=st.Page("pages/Upload_docs.py",title="Upload Your Documents",icon="📃")
+about=st.Page("pages/About_me.py",title="About me",icon="🌿")
+
+pg=st.navigation([Home,Upload,about])
+pg.run()
+
