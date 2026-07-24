4cc6e7406ed71cf47578e7aeeb218dc5adbf3db7
a25begdo <abdel-moujib.begdouri-terraf@imt-atlantique.net>
2026-07-24 11:38:21 +0100

    refactor: remove unused imports and clean up agent.py and db.py for improved readability

--- Rag/db.py
+++ Rag/db.py
@@ -1,10 +1,7 @@
 import chromadb as db
 import os
-from Rag import Chunking
-import uuid
-from chromadb.config import Settings
-import json
 
 vector_db=db.PersistentClient("Vectordb")
+
 def get_collec():
     return vector_db.get_or_create_collection("mrooc")
