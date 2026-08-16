341ec7b6c2201d75e13310b08f1107707d36ac39
a25begdo <abdel-moujib.begdouri-terraf@imt-atlantique.net>
2026-08-16 12:11:52 +0200

    fix: correct file extension checks in reset.py for accurate file deletion

--- files/reset.py
+++ files/reset.py
@@ -3,11 +3,11 @@ import os
 i=os.listdir("files/drug_text_files/")
 j=os.listdir("files/drug_json_files/")
 for file in i :
-    if file[-3:]=="txt":
+    if file[-4:]==".txt":
         os.remove("files/drug_text_files/"+ file)
 
 for file in j :
-    if file[-3:]=="json":
+    if file[-5:]==".json":
         os.remove("files/drug_json_files/"+ file)
         
 
