e456dd241da4c9c8f1b28b4c8c04735dcf16c28c
a25begdo <abdel-moujib.begdouri-terraf@imt-atlantique.net>
2026-08-18 11:35:41 +0200

    refactor : reorganize files app and assets strucutre

--- app/pages/app.py
+++ app/pages/app.py
@@ -7,7 +7,7 @@ Streamlit interface, wired to match your existing files:
 
 HOW TO RUN:
     pip install streamlit google-genai python-dotenv chromadb
-    streamlit run app.py
+    streamlit run app/main.py
 
 REQUIREMENTS:
     - Vectordb/ must already exist (run initialize_db.py once before this)
@@ -18,11 +18,10 @@ from pathlib import Path
 import sys
 
 
-sys.path.insert(0, str(Path(__file__).parent.parent))
-
+sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 from Rag import agent
 
-st.set_page_config(page_title="MedLeaf", page_icon="app/Gemini_Generated_Image_8yo9v28yo9v28yo9.png", layout="centered")
+st.set_page_config(page_title="MedLeaf", page_icon="app/assets/images/Gemini_Generated_Image_8yo9v28yo9v28yo9.png", layout="centered")
 
 
 # ──────────────────────────────────────────────────────────────────────────
@@ -37,16 +36,16 @@ if "display_log" not in st.session_state:
 # UI
 # ──────────────────────────────────────────────────────────────────────────
 
-st.image("app/Gemini_Generated_Image_pjw0i6pjw0i6pjw0.png", use_container_width=True)
-st.logo("app/Gemini_Generated_Image_pjw0i6pjw0i6pjw0.png",size="large")
-st.caption("Conversational RAG Agent for Drug Leaflet Understanding — talk to Mia")
+st.image("app/assets/images/Gemini_Generated_Image_pjw0i6pjw0i6pjw0.png", use_container_width=True)
+st.logo("app/assets/images/Gemini_Generated_Image_pjw0i6pjw0i6pjw0.png",size="large")
+st.caption("Conversational RAG Agent for Drug Leaflet Understanding - talk to Mia")
 st.divider()
 
-st.chat_message("assistant",avatar="app/female-doctor-to-explain-svgrepo-com.svg").write("Helloo, I am Mia, how can I help you today?")
+st.chat_message("assistant",avatar="app/assets/images/female-doctor-to-explain-svgrepo-com.svg").write("Helloo, I am Mia, how can I help you today?")
 
 for q, a, chunks in st.session_state.display_log:
     st.chat_message("user").write(q)
-    with st.chat_message("assistant",avatar="app/female-doctor-to-explain-svgrepo-com.svg"):
+    with st.chat_message("assistant",avatar="app/assets/images/female-doctor-to-explain-svgrepo-com.svg"):
         st.write(a)
         with st.expander("View retrieved chunks"):
             for i, c in enumerate(chunks, 1):
@@ -57,7 +56,7 @@ query = st.chat_input("Ask Mia about a medication...")
 
 if query:
     st.chat_message("user").write(query)
-    with st.chat_message("assistant",avatar="app/female-doctor-to-explain-svgrepo-com.svg"):
+    with st.chat_message("assistant",avatar="app/assets/images/female-doctor-to-explain-svgrepo-com.svg"):
         with st.spinner("Mia is reading the leaflets..."):
             answer, chunks = agent.ask_mia(query, st.session_state.history)
             st.write(answer)
@@ -70,5 +69,5 @@ if query:
     st.session_state.display_log.append((query, answer, chunks))
 
 st.divider()
-st.caption("MedLeaf · FDA API (batch) → Chunking.py → ChromaDB → Gemini")
+st.caption("MedLeaf · (FDA API + Uploaded documents) → Chunking.py → ChromaDB → Gemini")
 
